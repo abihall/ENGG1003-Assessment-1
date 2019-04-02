a51aa456adc25277ae4d9736056f6e561d1f1bf4
Abigail Hall <abi.hall018@gmail.com>
2019-04-02 01:47:00 +0000

    edit at mid-way through week 6 lab

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-    
-    return 0;
+   char letter[100];
+   int rotation_amount, i=0;
+   printf("Enter a message to encrypt:\n");
+	scanf("%s", letter);
+	printf("Enter rotation amount:\n ");
+	scanf("%d", &rotation_amount);
+
+    if(letter[i]>='a'&& letter[i]<='z'){ //dealing with only uppercase.
+        letter[i]=letter[i]-32;          //the 'A' and 'a' are a value of 32 apart
+    }
 }
