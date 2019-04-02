c60abfa5889ef8765f1ce8792b8cd09a35db1482
Abigail Hall <abi.hall018@gmail.com>
2019-04-02 02:41:54 +0000

    TASK 1, Without Space Consideration.

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+ 	
 
 int main()
 {
-   char letter[100];
-   int rotation_amount, i=0;
-   printf("Enter a message to encrypt:\n");
+    char letter[100];
+	int i, rotation_amount;
+	printf("Enter a message to encrypt:\n");
 	scanf("%s", letter);
 	printf("Enter rotation amount:\n ");
 	scanf("%d", &rotation_amount);
+	   
+	for(i = 0; letter[i] != '\0'; ++i){
+	
+	
+	    
+		if(letter[i] >= 'A' && letter[i] <= 'Z'){
+			letter[i] = (letter[i]-'A' + rotation_amount)%26+'A';
+	   }
 
-    if(letter[i]>='a'&& letter[i]<='z'){ //dealing with only uppercase.
-        letter[i]=letter[i]-32;          //the 'A' and 'a' are a value of 32 apart
-    }
+		else if(letter[i]>='a' && letter[i]<='z') {
+		    letter[i]=letter[i]-32;
+		 
+		}
+	}
+    
+	printf("Encrypted message: %s\n", letter);
+	return letter[i];
+	
 }
+
+
